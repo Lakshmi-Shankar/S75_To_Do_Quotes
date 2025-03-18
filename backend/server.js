a6b083d8c7c0fb80f55125d6d7821a460f171691
unknown <lakshmi7708671565@gmail.com>
2025-03-18 11:47:19 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
 app.use(cors()); 
@@ -17,9 +17,13 @@ app.get("/ping", (req, res) => {
     res.send("pong");
 });
 
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log("Database Connected Successfully")
+    app.listen(PORT,()=>{
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
+})
 
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
